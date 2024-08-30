6a845a4a5725d3a2695d401f7cc485135e8ad473
Petr Pivoňka <petrpiv1@gmail.com>
2024-08-30 04:54:19 +0200

    Add OpenTelemetry

--- ManiaServerManager/Program.cs
+++ ManiaServerManager/Program.cs
@@ -1,5 +1,8 @@
 using ManiaServerManager;
 using ManiaServerManager.Services;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 using System.IO.Abstractions;
 
 var builder = WebApplication.CreateSlimBuilder(args);
@@ -17,6 +20,39 @@ builder.Services.AddTransient<IServerStartService, ServerStartService>();
 builder.Services.AddTransient<ICliService, CliService>();
 builder.Services.AddTransient<IDedicatedCfgService, DedicatedCfgService>();
 builder.Services.AddHostedService<Startup>();
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.IncludeScopes = true;
+    options.IncludeFormattedMessage = true;
+    options.AddOtlpExporter();
+});
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(x =>
+    {
+        x.AddRuntimeInstrumentation()
+            .AddProcessInstrumentation()
+            .AddMeter("Microsoft.AspNetCore.Hosting")
+            .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+            .AddMeter("Microsoft.AspNetCore.Http.Connections")
+            .AddMeter("Microsoft.AspNetCore.Routing")
+            .AddMeter("Microsoft.AspNetCore.Diagnostics")
+            .AddMeter("Microsoft.AspNetCore.RateLimiting")
+            .AddMeter("System.Net.Http")
+            .AddOtlpExporter();
+    })
+    .WithTracing(x =>
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            x.SetSampler<AlwaysOnSampler>();
+        }
+
+        x.AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter();
+    });
 
 var app = builder.Build();
 
