7e347fa0eb6ae8847a6be81c2ef7fe8c74a3610c
Petr Pivoňka <petrpiv1@gmail.com>
2024-10-07 22:52:33 +0200

    Add HTTP resilience

--- ManiaServerManager/Program.cs
+++ ManiaServerManager/Program.cs
@@ -27,7 +27,7 @@ builder.Services.ConfigureHttpJsonOptions(options =>
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 });
 
-builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<IServerSetupService>().AddStandardResilienceHandler();
 builder.Services.AddTransient<IServerSetupService, ServerSetupService>();
 builder.Services.AddTransient<IZipExtractService, ZipExtractService>();
 builder.Services.AddTransient<IFileSystem, FileSystem>();
